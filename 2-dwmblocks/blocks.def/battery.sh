69195b5210bf9ecfe4c6a45cb572559faaeb3383
songwupei <songwupei@163.com>
2022-12-17 11:15:35 +0800

    delete siji font

--- 2-dwmblocks/blocks.def/battery.sh
+++ 2-dwmblocks/blocks.def/battery.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 ## use nf-mdi-* font by www.nerdfonts.com/cheat-sheet
-ICONn="" # icon for normal battery
-ICONc="" # icon for critical battery
+ICONbn="" # icon for normal battery
+ICONan="" # icon for normal battery
+ICONbc="" # icon for critical battery
 read -r capacity </sys/class/power_supply/BAT0/capacity
-printf "电池$ICONn%s%%" "$capacity"
+read -r ac </sys/class/power_supply/ADP0/online
 
 crit=15 # critical power_percent
-
-if [ "$capacity" -lt "$crit" ] ; then 
-		printf "充电$ICONc%s%%" "$capacity"
+if [ "$ac" = 1 ] ; then 
+    printf "电源$ICONan%s%%" "$capacity"
+elif  [ "$capacity" -lt "$crit" ] ; then 
+		printf "低电$ICONbc%s%%" "$capacity"
 		notify-send "请及时插入AC电源充电。"
 else
-		# printf "充电$ICONn%s%%" "$capacity"
-		# notify-send "good"
+		printf "电池$ICONbn%s%%" "$capacity"
 fi
--- 2-dwmblocks/blocks.def_/calendar_button.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-TERMINAL="st"
-case "$1" in
-    1) exec "$TERMINAL" -e calcurse ;;
-    # 2) exec "$TERMINAL" -e htop ;;
-    # 3) exec "$TERMINAL" -e htop -s PERCENT_MEM ;;
-esac
