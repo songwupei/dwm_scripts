7ad1964e64d3c99dff2ac025f15652c3a5bf431d
songwupei <songwupei@163.com>
2023-03-21 20:05:54 +0800

    clear dwmrc

--- 1-shell/dwm/dwmrc.zsh
+++ 1-shell/dwm/dwmrc.zsh
@@ -3,20 +3,15 @@
 ## 设置锁屏，并使用slock
 ## slock 快捷键为win(Mod4)+l
 xset s 300 &
-${HOME}/.config/dwm/xsidle.sh  slock & 
+${HOME}/.config/dwm/xsidle.sh slock & 
 
 # if connect the Personal  hotspot,open the nutstore web
 # else exec nutstore-daemon.py
-source "${HOME}/scripts/1-shell/nutstore_start.zsh"
-source "${HOME}/scripts/1-shell/networktool.zsh"
+exec nutstore &
+${HOME}/scripts/1-shell/networktool.zsh
 # source networktool.zsh
 essid=$(getessid) 
-essid_deny=("HUAWEIP40" "goodtime")
-if [[ ${essid_deny[(r)$(getessid)]} -lt $#essid ]] {
-nutstore_daemon -f web
-} else {
-nutstore_daemon -f client
-		}
+essid_deny=("HUAWEIP40" "goood")
 ## transparent
 exec picom &
 ## notify-send
@@ -26,7 +21,6 @@ exec dunst &
 exec dwmblocks &
 exec scroll &
 sh -c 'xinput --set-button-map "Logitech Wireless Mouse PID:4091" 3 2 1'
-
 ## start TLP
 sudo tlp start
 sudo systemctl mask systemd-rfkill.service
